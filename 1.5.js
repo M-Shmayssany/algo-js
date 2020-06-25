07a3c036d242df399106dfe83488b45abef23ccb
M-Shmayssany <m.shmayssany@gmail.com>
2020-06-25 14:43:23 +0200

    add 1.5.js

---
+++ 1.5.js
@@ -0,0 +1,7 @@
+const readlineSync = require("readline-sync");
+
+let numberA  = new Number(readlineSync.question('Please Enter a first decimal number A: '));
+let numberB  = new Number(readlineSync.question('Please Enter a second decimal number B:'));
+numberA = Math.round(numberA);
+NumberT = numberA * numberB;
+console.log(numberA + "the multiplication of A and B is: " + NumberT);
