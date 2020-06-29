031b2b7b0b5639ebe99b55fc43dca267838e8d9a
M-Shmayssany <m.shmayssany@gmail.com>
2020-06-29 12:24:40 +0200

    update

--- 5.2.js
+++ 5.2.js
@@ -1,6 +1,11 @@
 const readlineSync = require("readline-sync");
 
-let myObject = {}
+let myObject = {};
+let nextObject = {};
+
+function randomizeCast(tvSerie) {
+    tvSerie.sort(() => Math.random() - 0.5);
+  }
 
 function askTvSerie(){
     myObject.name = readlineSync.question("what is the name of your favorite TV serie: ");
@@ -12,7 +17,16 @@ function askTvSerie(){
         myObject.Names_cast_members.push({"name": Cname}); 
         Cname = readlineSync.question("what is the name of a cast member: ");
     }
+
+randomizeCast(myObject.Names_cast_members);
 console.log(JSON.stringify(myObject));
+
 }
 
 askTvSerie();
+
+nextObject.name = readlineSync.question("what is the name of your next TV serie: ");
+nextObject.Production_year = readlineSync.question("what is the Production year: ");
+nextObject.Names_cast_members = [];
+nextObject.Names_cast_members.push(myObject.Names_cast_members);
+console.log(JSON.stringify(nextObject));
